ab6ba08356a8bb66ccf1c87ab1f2ecf49d00c88a
lizavetion <alizafox2000@gmail.com>
2023-09-18 14:03:31 +0300

    Added Product cards (Mobile first)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,48 @@
 /*=============== LOADER ===============*/
 
-
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-    navToogle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
-if (navToogle){
-    navToogle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu')
-    })
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
-if (navClose){
-    navClose.addEventListener('click',() => {
-        navMenu.classList.remove('show-menu')
-    })
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-function scrollHeader(){
-    const header = document.getElementById('header')
-    if (this.scrollY >= 50) {
-        header.classList.add('scroll-header')
-    } else {
-        header.classList.remove('scroll-header')
-    }
+function scrollHeader() {
+  const header = document.getElementById("header");
+  if (this.scrollY >= 50) {
+    header.classList.add("scroll-header");
+  } else {
+    header.classList.remove("scroll-header");
+  }
 }
-window.addEventListener('scroll', scrollHeader)
+
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== MIXITUP FILTER PRODUCTS ===============*/
 
+/* Default filter products */
 
-/* Default filter products */ 
+/* Link active products */
 
-
-/* Link active products */ 
-
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
